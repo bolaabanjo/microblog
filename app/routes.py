225992d09b03af0fa75eb68c30c083738ae1fbfe
bolaabanjo <royadebanjo@gmail.com>
2025-09-03 15:01:30 +0100

    changes back from Bola to Roy

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,4 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Roy'}
-    return '''
-    <html>
-        <head>
-            <title>Home page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    return render_template('index.html', title='Home', user=user)
